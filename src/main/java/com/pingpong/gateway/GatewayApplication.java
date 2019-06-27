4ebf0b89920941d74ee202c4227509276345905d
Ilya <polushan@yandex.ru>
2019-06-27 19:03:13 +0400

    Migrate to cloud-gateway

--- src/main/java/com/pingpong/gateway/GatewayApplication.java
+++ src/main/java/com/pingpong/gateway/GatewayApplication.java
@@ -3,12 +3,8 @@ package com.pingpong.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-@EnableZuulProxy
-@EnableSwagger2
 @EnableEurekaClient
 @SpringBootApplication
 public class GatewayApplication {
